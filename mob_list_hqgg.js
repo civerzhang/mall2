4233f603cf387b38c216c4dd3630d4f45129ce28
tdxzhangsiwei <934599659@qq.com>
2018-02-02 19:26:32 +0800

    活期公告页面

---
+++ mob_list_hqgg.js
@@ -0,0 +1,85 @@
+var mob_list_hqgg = {
+  items: [
+    {
+      tplid: "mob-list-jjxq",
+      index: 2,
+      lines: [
+        {
+          title: "基金合同", 
+          urlParam: {
+            "OpenName": "基金合同",
+            "OpenUrl": "fhxx.html",
+            "queryParams": [
+              { key: "url", value: "ht" }
+            ],
+            "OpenParam": {
+              UrlType: "Remote"
+            },
+            "OpenType": __tdxMobSystem == "Android" ? "browser" : "native"
+          } 
+        },
+        {
+          title: "基金招募说明书", 
+          urlParam: {
+            "OpenName": "基金招募说明书",
+            "OpenUrl": "fhxx.html",
+            "queryParams": [
+              { key: "url", value: "zms" }
+            ],
+            OpenParam: {
+              UrlType: "Remote"
+            },
+            OpenType: __tdxMobSystem == "Android" ? "browser" : "native"
+          } 
+        },
+        { 
+          title: "风险揭示书", 
+          urlParam: {
+            "OpenName": "风险揭示书",
+            "OpenUrl": "fhxx.html",
+            "queryParams": [
+              { key: "url", value: "jss" }
+            ],
+            OpenParam: {
+              UrlType: "Remote"
+            },
+            OpenType: __tdxMobSystem == "Android" ? "browser" : "native"
+          } 
+        },
+        { 
+          title: "证券投资基金投资者风险提示函", 
+          urlParam: {
+            "OpenName": "证券投资基金投资者风险提示函",
+            "OpenUrl": "fhxx.html",
+            "queryParams": [
+              { key: "url", value: "tzzfxtsh" }
+            ],
+            "OpenParam": {
+              UrlType: "Remote"
+            }
+          } 
+        },
+        { 
+          title: "证券投资基金投资人权益须知", 
+          urlParam: {
+            "OpenName": "证券投资基金投资人权益须知",
+            "OpenUrl": "fhxx.html",
+            "queryParams": [
+              { key: "url", value: "txrqyxz" }
+            ],
+            "OpenParam": {
+              UrlType: "Remote"
+            }
+          } 
+        }
+      ],
+      lineStyle: {
+        borderBottom: "1px solid #ddd"
+      },
+      image: {
+        url: "arrow.png",
+        style: {}
+      }
+    },
+  ],
+}
